de2155cc0371bb81007b5aaa609be40f0e44e4d4
HatWithPins <daniel.carril4@gmail.com>
2020-04-06 11:16:23 +0200

    First upload from local repo.

---
+++ Capstone/Green/run_kubernetes.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Run the Docker Hub container with kubernetes
+kubectl apply -f ./green-controller.json
---
+++ Capstone/run_kubernetes.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Run the Docker Hub container with kubernetes
+kubectl apply -f ./blue-green-service.json
